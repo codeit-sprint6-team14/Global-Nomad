fcfe3f1779b545464b1b085d19f89946abb2c1bf
hoon <dudrn587@gmail.com>
2024-07-18 13:54:29 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import '@/styles/globals.css'
+
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
