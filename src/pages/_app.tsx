61ac6bcf57654e6f0919661236feff1918651450
Lin <heron6534@gmail.com>
2024-07-25 20:09:48 +0900

    [FP-7] Feat: 폰트 전역 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,19 @@
 import '@/styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={pretendard.variable}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
