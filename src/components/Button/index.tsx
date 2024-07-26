758375df33ad010db8175c8ac44bcdd230ff3ecd
Lin <heron6534@gmail.com>
2024-07-26 23:22:28 +0900

    [FP-20] Refactor: 버튼 공통 컴포넌트 리팩토링
    - variant, onClick props 추가
    - size, backgroundColor props 제거

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,59 +1,49 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/function-component-definition */
 
-interface ButtonProps {
-  disabled?: boolean
-  type: 'button' | 'submit' | 'reset' | undefined
-  className?: string
-  backgroundColor: string
-  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xlg'
-  children: string
+// interface ButtonProps {
+//   disabled?: boolean;
+//   type: 'button' | 'submit' | 'reset' | undefined;
+//   className?: string;
+//   backgroundColor: string;
+//   children: string;
+// }
+
+type ButtonVariant = 'primary' | 'secondary';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
 }
 
 const Button = ({
   disabled = false,
   type = 'button',
-  className,
-  backgroundColor = 'black',
-  size,
+  className = '',
+  variant = 'primary',
   children,
+  onClick,
+  ...rest
 }: ButtonProps) => {
-  const baseStyle = 'text-lg-bold w-350 h-48 rounded-6'
-  const disabledStyle = 'bg-gray-600 cursor-not-allowed text-white'
-  const backgroundStyle =
-    backgroundColor === 'black'
-      ? 'bg-black-100 text-white'
-      : 'bg-white text-black-100 border border-black-100'
-
-  let variantStyle = ''
-  switch (size) {
-    case 'xs':
-      variantStyle = 'font-bold leading-6 text-sm w-95 h-35'
-      break
-    case 'sm':
-      variantStyle = 'text-md-bold w-108 h-38'
-      break
-    case 'md':
-      variantStyle = 'text-lg-bold w-144 h-48'
-      break
-    case 'lg':
-      variantStyle = 'text-lg-bold w-350 h-48'
-      break
-    case 'xlg':
-      variantStyle = 'text-lg-bold w-640 h-48'
-      break
-  }
+  const baseStyle = 'text-lg-bold w-350 h-48 rounded-6';
+  const disabledStyle = 'bg-gray-600 cursor-not-allowed text-white';
+  const variantStyles: Record<ButtonVariant, string> = {
+    primary: 'bg-black-100 text-white',
+    secondary: 'bg-white text-black-100 border border-black-100',
+  };
 
   return (
     <div>
       <button
         disabled={disabled}
         type={type}
-        className={`${baseStyle} ${disabled ? disabledStyle : backgroundStyle} ${variantStyle} ${className}`}
+        className={`${baseStyle} ${disabled ? disabledStyle : variantStyles[variant]} ${className}`}
+        onClick={onClick}
+        {...rest}
       >
         {children}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
