2ebb1568b892a7e600942bf60d20d9ecb7100f45
JaesungKo <jaesungko.dev@gmail.com>
2024-07-27 22:09:21 +0900

    [FP-19] Design: DropDownInput 컴포넌트 구현

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,6 +1,10 @@
 import DefaultInput from './DefaultInput'
 import PasswordInput from './PasswordInput'
+import DropDownInput from './DropDownInput'
 
-const Input = Object.assign(DefaultInput, { Password: PasswordInput })
+const Input = Object.assign(DefaultInput, {
+  Password: PasswordInput,
+  DropDown: DropDownInput,
+})
 
 export default Input
