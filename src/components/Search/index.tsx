01368ecd7a889abe158aeafba351841e3c4ef432
YeongTaek <yeongtaek2109@gmail.com>
2024-07-27 15:19:40 +0900

    [FP-6] Design: Search UI

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Search = () => {
+  return (
+    <div className="box-shadow: 0px 4px 16px 0px #1122110D flex h-129 w-343 flex-col gap-15 rounded-16 border px-24 py-16 md:h-166 md:w-696 md:gap-20 md:py-32 lg:h-178 lg:w-1200 lg:gap-32">
+      <label htmlFor="search" className="text-lg-bold md:text-xl-bold">
+        무엇을 체험하고 싶으신가요?
+      </label>
+      <div className="flex h-56 w-295 items-center gap-16 md:w-648 md:gap-10 lg:w-1154">
+        <div className="relative flex w-187 gap-10 md:w-500 lg:w-1004">
+          <Image
+            src="/images/icon-search.svg"
+            alt="searchIcon"
+            className="absolute left-3 top-[15%]"
+            width={40}
+            height={40}
+          />
+          <input
+            type="text"
+            id="search"
+            name="search"
+            placeholder=""
+            className="h-56 w-full rounded-4 border border-gray-700 pl-[22%] text-md-regular md:pl-[8%] lg:pl-[4%]"
+          />
+          <p>내가 원하는 체험은</p>
+        </div>
+        <button
+          type="button"
+          className="h-56 w-96 rounded-4 bg-black-100 px-15 py-8 text-lg-bold text-white md:w-136"
+        >
+          검색하기
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Search;
