2bc63909a5c1c887935eaae7ca6322bafea2309c
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-07-28 19:31:40 +0900

    [FP-21] Design: 체험 카드 리소스 디자인(반응형까지)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     <div>
       <NavBar />
       <CardResource.popularExperienceCard />
+      <CardResource.standardExperienceCard />
       <Footer />
     </div>
   );
