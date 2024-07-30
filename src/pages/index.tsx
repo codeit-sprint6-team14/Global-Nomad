aa3fb892b1e51958c196a0dc97282105a1e4c69d
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-07-30 15:46:02 +0900

    [FP-21] Style: 테스트 코드 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,11 @@
 import Footer from '@/components/Footer';
 import NavBar from '@/components/NavBar';
-import CardResource from '@/components/CardResource';
 //import SideNavMenu from '@/components/SideNavMenu';
-
-const CardResourceData = [
-  {
-    image: '/images/flower.jpeg',
-    rating: 4.9,
-    reviewCount: 150,
-    title: '창의력을 키우는 아트 클래스',
-    price: 130000,
-  },
-];
 
 export default function Home() {
   return (
     <div>
       <NavBar />
-      {CardResourceData.map((data, index) => (
-        <CardResource.popularExperienceCard
-          key={index}
-          image={data.image}
-          rating={data.rating}
-          reviewCount={data.reviewCount}
-          title={data.title}
-          price={data.price}
-        />
-      ))}
-      {CardResourceData.map((data, index) => (
-        <CardResource.standardExperienceCard
-          key={index}
-          image={data.image}
-          rating={data.rating}
-          reviewCount={data.reviewCount}
-          title={data.title}
-          price={data.price}
-        />
-      ))}
       <Footer />
     </div>
   );
