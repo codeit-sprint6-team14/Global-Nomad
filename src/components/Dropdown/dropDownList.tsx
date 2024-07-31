c1714c9046735fe4af5564ea89e3749238486846
Lin <heron6534@gmail.com>
2024-07-31 16:53:10 +0900

    [FP-38] Refactor: DropDown 공통 컴포넌트 적용

--- src/components/Dropdown/dropDownList.tsx
+++ src/components/Dropdown/dropDownList.tsx
@@ -1,18 +1,8 @@
 import { ReactNode } from 'react';
 
-const DropDownList = ({
-  children,
-  classNames,
-}: {
-  children: ReactNode;
-  classNames: string;
-}) => {
+const DropDownList = ({ children, classNames }: { children: ReactNode; classNames: string }) => {
   return (
-    <ul
-      className={`${classNames} absolute right-[-12px] top-40 z-50 rounded-6 border border-solid border-gray-300 bg-white`}
-    >
-      {children}
-    </ul>
+    <ul className={`${classNames} absolute rounded-6 border border-solid border-gray-300 bg-white`}>{children}</ul>
   );
 };
 
