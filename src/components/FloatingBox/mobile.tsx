161076486f10b3916f48d9ef6d63b688ae269442
Lin <heron6534@gmail.com>
2024-07-31 14:55:32 +0900

    [FP-32] Refactor: 태그변경
    - footer 태그를 div 태그로 변경

--- src/components/FloatingBox/mobile.tsx
+++ src/components/FloatingBox/mobile.tsx
@@ -7,7 +7,7 @@ const Mobile = () => {
   };
 
   return (
-    <footer className="fixed bottom-0 left-0 right-0 h-83 w-full max-w-[743px] border-t border-gray-300 bg-white">
+    <div className="fixed bottom-0 left-0 right-0 h-83 w-full max-w-[743px] border-t border-gray-300 bg-white">
       <div className="flex items-center justify-between px-16 py-16">
         <div className="flex flex-col">
           <MobileComponents.PriceInfo price={10000} capacity={10} />
@@ -17,7 +17,7 @@ const Mobile = () => {
           예약하기
         </Button>
       </div>
-    </footer>
+    </div>
   );
 };
 
