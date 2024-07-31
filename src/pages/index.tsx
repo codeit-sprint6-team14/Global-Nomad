2a2c6c9a2be567b4b0d8b4bfee29f5d3ddfb1cba
Lin <heron6534@gmail.com>
2024-07-31 16:07:41 +0900

    [FP-36] Chore: index.tsx 내 컴포넌트 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-// import Footer from '@/components/Footer';
-import MyActivityCardList from '@/components/Cards/myActivityCardList';
-import ReservationCardList from '@/components/Cards/reservationCardList';
-import NavBar from '@/components/NavBar';
-import SideNavMenu from '@/components/SideNavMenu/index';
-
 export default function Home() {
-  return (
-    <div>
-      <SideNavMenu />
-      <MyActivityCardList />
-      <NavBar />
-      <ReservationCardList />
-      {/* <Footer /> */}
-    </div>
-  );
+  return <div></div>;
 }
