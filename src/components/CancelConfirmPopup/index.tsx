0cdaeab2947f0ddd5b60c230699a18ad847aaacc
YeongTaek <yeongtaek2109@gmail.com>
2024-07-31 22:52:43 +0900

    [FP-34] Style: Button 컴포넌트 사용

--- src/components/CancelConfirmPopup/index.tsx
+++ src/components/CancelConfirmPopup/index.tsx
@@ -1,19 +1,21 @@
+import Image from 'next/image';
+
 import Button from '../Button';
 
 const CancelConfirmPopup = () => {
   return (
     <div className="flex h-184 w-288 flex-col items-center justify-evenly border shadow-modal">
       <div className="flex flex-col items-center justify-between gap-16">
-        icon
+        <Image src="/svgs/ic-check.svg" width={24} height={24} alt="" />
         <p className="text-lg-regular text-black">예약을 취소하시겠어요?</p>
       </div>
       <div className="flex gap-8">
-        <Button variant="secondary" className="h-38 w-80">
+        <Button.Default variant="secondary" className="h-38 w-80">
           아니오
-        </Button>
-        <Button type="submit" className="h-38 w-80">
+        </Button.Default>
+        <Button.Default type="submit" className="h-38 w-80">
           취소하기
-        </Button>
+        </Button.Default>
       </div>
     </div>
   );
--- src/components/RegisterConfirmPopup/index.tsx
+++ src/components/RegisterConfirmPopup/index.tsx
@@ -10,7 +10,9 @@ const RegisterConfirmPopup = ({ children }: PopupProps) => {
       <span className="absolute top-[35%] text-lg-medium text-black-100 md:top-[40%] md:text-2lg-medium">
         {children}
       </span>
-      <Button className="absolute bottom-20 right-[30%] h-42 sm:w-138 md:right-30 md:h-48 md:w-120">확인</Button>
+      <Button.Default className="absolute bottom-20 right-[30%] h-42 sm:w-138 md:right-30 md:h-48 md:w-120">
+        확인
+      </Button.Default>
     </div>
   );
 };
