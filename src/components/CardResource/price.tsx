507a096065e622e86b1061466349a641ae3c3967
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2024-07-31 15:22:48 +0900

    [FP-33] Refactor: price에 쉼표 추가

--- src/components/CardResource/price.tsx
+++ src/components/CardResource/price.tsx
@@ -6,7 +6,7 @@ interface PriceProps {
 const Price: React.FC<PriceProps> = ({ price, className = '' }) => {
   return (
     <p className={`text-xl-bold ${className}`}>
-      ₩ {price}
+      ₩ {price.toLocaleString('ko-KR')}
       <span className="mx-5 text-lg-regular text-gray-500 md:text-xl-regular">/ 인</span>
     </p>
   );
