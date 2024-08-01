d727f4616499ef8e848780d61b2907c3106b0adc
YeongTaek <yeongtaek2109@gmail.com>
2024-08-02 04:59:06 +0900

    [FP-27] Feat: TabButton 컴포넌트 분리 및 동적

---
+++ src/components/ReservationInfoModal/components/tabButton.tsx
@@ -0,0 +1,39 @@
+export type TabType = '신청' | '확정' | '거절';
+
+export type TabData = {
+  type: TabType;
+  count: number;
+};
+
+type TabButtonsProps = {
+  tabData: TabData[];
+  selectedTab: TabType;
+  onTabClick: (tab: TabType) => void;
+};
+
+const TabButton = ({ type, count, isSelected, onClick }: TabData & { isSelected: boolean; onClick: () => void }) => (
+  <button
+    onClick={onClick}
+    className={`relative ${
+      isSelected ? 'border-b-4 border-green-300 text-xl-semibold text-green-300' : 'text-xl-regular text-gray-800'
+    }`}
+  >
+    {type} {count}
+  </button>
+);
+
+const defaultTabs: TabType[] = ['신청', '확정', '거절'];
+
+const TabButtons: React.FC<TabButtonsProps> = ({ tabData = [], selectedTab, onTabClick }) => {
+  const tabMap = new Map(tabData.map((tab) => [tab.type, tab]));
+  return (
+    <div className="flex gap-12">
+      {defaultTabs.map((type) => {
+        const tab = tabMap.get(type) || { type, count: 0 };
+        return <TabButton key={type} {...tab} isSelected={selectedTab === type} onClick={() => onTabClick(type)} />;
+      })}
+    </div>
+  );
+};
+
+export default TabButtons;
