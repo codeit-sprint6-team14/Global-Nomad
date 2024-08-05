c06fcc71aa6a2de20392a0652bac077176624393
Lin <heron6534@gmail.com>
2024-08-05 12:07:03 +0900

    Test: 공통 컴포넌트 Storybook 적용 (#35)
    * [FP-46] Remove: 불필요한 파일 삭제
    
    - story book 예제 파일 삭제
    
    * [FP-46] Feat: storybook에 적용된 webpack 모듈 설정 변경
    
    * [FP-46] Feat: global.css 불러오기
    
    * [FP-46] Chore: availableSchedule mock data 수정
    
    * [FP-46] Test: Floating Box Story book 적용
    
    * [FP-46] Test: Button Story book 적용
    
    * [FP-46] Test: Calendar Story book 적용
    
    - Story book 적용 후 에러 해결
    
    * [FP-46] Test: Dropdown Story book 적용
    
    * [FP-46] Test: Chip Story book 적용
    
    * [FP-46] Test: Counter Story book 적용
    
    * [FP-46] Test: Date Select Modal Story book 적용
    
    * [FP-46] Fix:  빌드 에러 해결

--- src/components/Dropdown/dropDownOption.tsx
+++ src/components/Dropdown/dropDownOption.tsx
@@ -7,13 +7,13 @@ const DropDownOption = ({
   key?: string | number;
   handleOptionClick: (label: string) => void;
   label: string;
-  className: string;
+  className?: string;
 }) => {
   return (
     <li
       key={key}
       onClick={() => handleOptionClick(label)}
-      className={`w-full cursor-pointer border-b border-gray-300 px-16 py-12 text-center text-md-medium text-gray-800 hover:bg-green-100 ${className}`}
+      className={`w-full cursor-pointer border-b border-gray-300 px-16 py-12 text-center text-md-medium text-gray-800 last:border-b-0 hover:bg-green-100 ${className}`}
     >
       {label}
     </li>
