e21f73b6a447b0a516d3e415dab99fc48ddb7628
YeongTaek <yeongtaek2109@gmail.com>
2024-08-05 18:44:11 +0900

    [FP-27] Docs: shadow-popover 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,6 +30,7 @@ const config: Config = {
       },
       boxShadow: {
         modal: '0px 4px 16px 0px rgba(17, 34, 17, 0.05)',
+        popover: '0px 2px 8px 0px rgba(120, 116, 134, 0.25)',
       },
       borderRadius: PX0_50,
       fontSize: {
