c89cf12972002e7d3b21f27d08a60a7f35ed8c63
Lin <heron6534@gmail.com>
2024-08-07 17:20:13 +0900

    [FP-55] Feat: Review 모달 닫기 기능 구현

--- src/components/common/Modal/index.ts
+++ src/components/common/Modal/index.ts
@@ -1,9 +1,11 @@
+import CancelReservation from './CancelReservation';
 import DateSelect from './DateSelect';
 import Review from './Review';
 
 const Modal = {
   DateSelect: DateSelect,
   Review: Review,
+  CancelReservation: CancelReservation,
 };
 
 export default Modal;
