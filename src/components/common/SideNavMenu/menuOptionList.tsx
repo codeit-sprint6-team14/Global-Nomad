e53b3f513c695a04c1334d140afab0ec58c312e1
Lin <heron6534@gmail.com>
2024-08-07 17:22:16 +0900

    [FP-55] Feat: 사이드 네비게이션 바 path 속성 추가

--- src/components/common/SideNavMenu/menuOptionList.tsx
+++ src/components/common/SideNavMenu/menuOptionList.tsx
@@ -7,6 +7,7 @@ interface MenuOptionListProps {
   id: number;
   imgSrc: JSX.Element;
   text: string;
+  path: string;
 }
 
 const menuOptionList: MenuOptionListProps[] = [
@@ -14,21 +15,25 @@ const menuOptionList: MenuOptionListProps[] = [
     id: 1,
     imgSrc: <MyInformation />,
     text: '내 정보',
+    path: '/my-page/profile',
   },
   {
     id: 2,
     imgSrc: <BookingHistory />,
     text: '예약 내역',
+    path: '/my-page/reservation-list',
   },
   {
     id: 3,
     imgSrc: <Setting />,
     text: '내 체험 관리',
+    path: '/my-page/activity-settings',
   },
   {
     id: 4,
     imgSrc: <BookingStatus />,
     text: '예약 현황',
+    path: '/my-page/schedule',
   },
 ];
 
