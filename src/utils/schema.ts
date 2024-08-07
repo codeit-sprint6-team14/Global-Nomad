fee07bd4598fafd4c52490b5ee628a9e98a7eb80
JaesungKo <jaesungko.dev@gmail.com>
2024-08-07 21:56:06 +0900

    Feat: 유효성 검사 스키마 구현

---
+++ src/utils/schema.ts
@@ -0,0 +1,19 @@
+import ErrorMessages from '@/constants/errorMessages';
+import * as yup from 'yup';
+
+export const signupValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .required(ErrorMessages.EMAIL_REQUIRED)
+    .matches(/^\w+([.-]?\w+)@\w+([.-]?\w+)(\.\w{2,3})+$/, ErrorMessages.INVALID_EMAIL),
+  password: yup.string().required(ErrorMessages.PASSWORD_REQUIRED).min(8, ErrorMessages.INVALID_PASSWORD),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref('password')], ErrorMessages.PASSWORD_CONFIRMATION_MISMATCH)
+    .required(ErrorMessages.PASSWORD_CONFIRMATION_REQUIRED),
+  nickname: yup
+    .string()
+    .required(ErrorMessages.NICKNAME_REQUIRED)
+    .max(10, ErrorMessages.NICKNAME_TOO_LONG)
+    .matches(/^[가-힣a-zA-Z0-9]+$/, ErrorMessages.NICKNAME_NO_SPECIAL_CHARS),
+});
