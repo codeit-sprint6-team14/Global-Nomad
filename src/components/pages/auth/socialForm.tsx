2c8f4af8c9747cfab812115123a77aadc6839b02
Jaesung Ko <98534731+devjaesung@users.noreply.github.com>
2024-08-09 15:12:47 +0900

    Design: 로그인/회원가입 페이지 UI 구현 (#46)
    * [FP-65] Design: signup페이지 UI 구현
    
    * Feat: 유효성 검사 스키마 구현
    
    * Feat: 에러 메세지 상수화
    
    * [FP-65] Feat: 회원가입 페이지 유효성 검사 적용
    
    * [FP-65] Feat: 로그인 페이지 유효성 검사 적용
    
    * [FP-65] Fix: Image태그 관련 콘솔 에러 해결
    
    * [FP-65] Design: 소셜 로그인 UI 구현
    
    * [FP-65] Design: UI 수정 및 hover시 색상 변경 적용

---
+++ src/components/pages/auth/socialForm.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+
+interface Props {
+  text: string;
+}
+
+const SocialForm = ({ text }: Props) => {
+  return (
+    <>
+      <div className="flex items-center sm:mb-24 sm:mt-40 sm:w-350 md:mb-40 md:mt-48 md:w-640">
+        <div className="h-1 w-full grow bg-gray-300"></div>
+        <span className="shrink-0 text-gray-700 sm:px-18 sm:text-md-regular md:px-30 md:text-xl-regular">{text}</span>
+        <div className="h-1 w-full grow bg-gray-300"></div>
+      </div>
+      <div className="flex justify-center gap-20">
+        <div className="relative flex items-center justify-center rounded-full border border-gray-300 hover:border-green-300 sm:h-48 sm:w-48 md:h-72 md:w-72">
+          <Image
+            src="/assets/icons/google.svg"
+            alt="google"
+            fill
+            className="absolute sm:p-14 sm:hover:p-12 md:p-22 md:hover:p-20"
+          />
+        </div>
+        <div className="relative flex items-center justify-center rounded-full border border-gray-300 hover:border-green-300 sm:h-48 sm:w-48 md:h-72 md:w-72">
+          <Image
+            src="/assets/icons/kakaotalk.svg"
+            alt="kakao talk"
+            fill
+            className="absolute sm:p-14 sm:hover:p-12 md:p-22 md:hover:p-20"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SocialForm;
