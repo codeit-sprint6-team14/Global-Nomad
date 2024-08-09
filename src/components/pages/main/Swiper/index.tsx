706d120d6c37f6c64523f849114b7885480acd39
YeongTaek <yeongtaek2109@gmail.com>
2024-08-10 01:26:55 +0900

    [FP-66] Feat: Swiper 구현

---
+++ src/components/pages/main/Swiper/index.tsx
@@ -0,0 +1,11 @@
+import SwiperRoot from './SwiperRoot';
+import SwiperSlide from './SwiperSlide';
+import SwiperTrigger from './SwiperTrigger';
+
+const Swiper = {
+  SwiperRoot: SwiperRoot,
+  SwiperTrigger: SwiperTrigger,
+  SwiperSlide: SwiperSlide,
+};
+
+export default Swiper;
