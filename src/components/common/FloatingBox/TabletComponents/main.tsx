087575828e214a40f705ac806437cdc1492601a2
hoon <dudrn587@gmail.com>
2024-08-10 20:57:14 +0900

    [FP-59] Refactor: 체험 상세 페이지 테블릿 캘린더 예약하기 post 로직 위치 변경

--- src/components/common/FloatingBox/TabletComponents/main.tsx
+++ src/components/common/FloatingBox/TabletComponents/main.tsx
@@ -1,6 +1,9 @@
 import Button from '@/components/common/Button';
 import Counter from '@/components/common/Counter';
 import Modal from '@/components/common/Modal';
+import { useActivityReservationMutation } from '@/hooks/useReservationMutation';
+import { activityIdAtom, formSubmitDataAtom } from '@/store/activityDetailsAtom';
+import { useAtomValue } from 'jotai';
 import { useState } from 'react';
 
 const Main = () => {
@@ -9,6 +12,14 @@ const Main = () => {
   const handleDateSelectModal = () => {
     // TODO: 모달 여닫는 기능 구현
     setIsOpenModal((prev) => !prev);
+  };
+
+  const { submitReservation } = useActivityReservationMutation();
+  const activityId = useAtomValue(activityIdAtom);
+  const submitFormData = useAtomValue(formSubmitDataAtom);
+
+  const handleReservationFormSubmit = () => {
+    submitReservation(activityId, submitFormData);
   };
 
   return (
@@ -23,7 +34,9 @@ const Main = () => {
         <div className="mb-32">
           <Counter />
         </div>
-        <Button.Default className="h-56 w-203">예약하기</Button.Default>
+        <Button.Default className="h-56 w-203" onClick={handleReservationFormSubmit}>
+          예약하기
+        </Button.Default>
       </main>
     </div>
   );
--- src/components/common/Modal/DateSelect/footer.tsx
+++ src/components/common/Modal/DateSelect/footer.tsx
@@ -1,25 +1,14 @@
 import Button from '@/components/common/Button';
-import { useActivityReservationMutation } from '@/hooks/useReservationMutation';
-import { activityIdAtom, formSubmitDataAtom } from '@/store/activityDetailsAtom';
-import { useAtomValue } from 'jotai';
 
 interface FooterProps {
   isReservationPossible: boolean;
 }
 
 const Footer = ({ isReservationPossible }: FooterProps) => {
-  const { submitReservation } = useActivityReservationMutation();
-  const activityId = useAtomValue(activityIdAtom);
-  const submitFormData = useAtomValue(formSubmitDataAtom);
-
-  const handleReservationFormSubmit = () => {
-    submitReservation(activityId, submitFormData);
-  };
-
   return (
     <div>
       <footer>
-        <Button.Default disabled={!isReservationPossible} className="h-56 w-432" onClick={handleReservationFormSubmit}>
+        <Button.Default disabled={!isReservationPossible} className="h-56 w-432">
           확인
         </Button.Default>
       </footer>
