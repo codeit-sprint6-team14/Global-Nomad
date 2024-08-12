6915a974105f6f39686d33562d8ec18e09f27a85
Lin <heron6534@gmail.com>
2024-08-12 14:18:21 +0900

    [FP-70] Feat: Default Input에 forwardRef 추가

--- src/components/common/Input/DefaultInput.tsx
+++ src/components/common/Input/DefaultInput.tsx
@@ -1,27 +1,21 @@
 import { InputProps } from '@/types/defaultInputTypes';
+import React, { forwardRef } from 'react';
 
-const DefaultInput = ({
-  type = 'text',
-  className,
-  error = false,
-  placeholder = '입력',
-  isAuth = false,
-  register,
-  ...props
-}: InputProps) => {
-  const baseStyle = `
-    block w-full outline-none placeholder:text-gray-600 text-14 md:text-16 focus:border-[1.5px]
-    ${isAuth ? 'rounded-6 h-58 border-gray-600' : 'border-gray-700 rounded-4 h-56'}
-    pl-16 pr-40 border
-  `;
-  const normalBorder = 'focus:border-green-300';
-  const errorBorder = 'border-red-500 focus:border-red-200';
+const DefaultInput = forwardRef<HTMLInputElement, InputProps>(
+  ({ type = 'text', className, error = false, placeholder = '입력', isAuth = false, ...props }, ref) => {
+    const baseStyle = `
+      block w-full outline-none placeholder:text-gray-600 text-lg-regular md:text-16 focus:border-[1.5px]
+      ${isAuth ? 'rounded-6 h-58 border-gray-600' : 'border-gray-700 rounded-4 h-56'}
+      pl-16 pr-40 border
+    `;
+    const styleClass = `${baseStyle} ${error ? 'errorBorder' : 'normalBorder'}`;
 
-  const styleClass = `${baseStyle} ${error ? errorBorder : normalBorder}`;
-
-  return (
-    <input className={`${styleClass} ${className}`} type={type} placeholder={placeholder} {...register} {...props} />
-  );
-};
+    return (
+      <input ref={ref} className={`${styleClass} ${className}`} type={type} placeholder={placeholder} {...props} />
+    );
+  },
+);
 
 export default DefaultInput;
+
+DefaultInput.displayName = 'DefaultInput';
