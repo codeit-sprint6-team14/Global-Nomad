1d4466b5b41792131543aee338656910dba77873
Lin <heron6534@gmail.com>
2024-08-12 14:39:16 +0900

    [FP-70] Feat: 체험 등록 mock data 추가

---
+++ src/mockData/registActivity.js
@@ -0,0 +1,36 @@
+const registActivityData = {
+  title: '함께 배우면 즐거운 스트릿댄스',
+  category: '투어',
+  description: '둠칫 둠칫 두둠칫',
+  address: '서울특별시 강남구 테헤란로 427',
+  price: 10000,
+  schedules: [
+    {
+      date: '2023-12-01',
+      startTime: '12:00',
+      endTime: '13:00',
+    },
+    {
+      date: '2023-12-05',
+      startTime: '12:00',
+      endTime: '13:00',
+    },
+    {
+      date: '2023-12-05',
+      startTime: '13:00',
+      endTime: '14:00',
+    },
+    {
+      date: '2023-12-05',
+      startTime: '14:00',
+      endTime: '15:00',
+    },
+  ],
+  bannerImageUrl:
+    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/a.png',
+  subImageUrls: [
+    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/activity_registration_image/b.png',
+  ],
+};
+
+export default registActivityData;
