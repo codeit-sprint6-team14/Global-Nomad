9d7bcca2ee724494855c268e2af4c76dc37124ff
Lin <heron6534@gmail.com>
2024-08-12 14:19:01 +0900

    [FP-70] Feat: customInput에 상태값 추가
    - isFocused 상태값 추가

--- src/components/common/Input/DateInput/customInput.tsx
+++ src/components/common/Input/DateInput/customInput.tsx
@@ -1,26 +1,42 @@
 import { CustomInputProps } from '@/types/dateInput';
 import Image from 'next/image';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 
 const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(
-  ({ value, onClick, placeholder, className }, ref) => (
-    <div className={`relative sm:w-130 md:w-149 lg:w-379 ${className}`}>
-      <input
-        type="text"
-        className="w-full cursor-pointer rounded-4 border border-gray-700 py-5 focus:outline-none focus:ring-1 focus:ring-black-100 sm:h-40 sm:px-12 md:h-56 md:px-16"
-        onClick={onClick}
-        value={value}
-        ref={ref}
-        placeholder={placeholder}
-        readOnly
-      />
-      <div className="pointer-events-none absolute top-1/2 -translate-y-1/2 sm:right-8 md:right-15">
-        <Image src="/icons/calendar.svg" alt="calendar" width={24} height={24} />
+  ({ value, onClick, placeholder, className }, ref) => {
+    const [isFocused, setIsFocused] = useState(false);
+
+    const handleFocus = () => {
+      setIsFocused(true);
+
+      if (onClick) onClick();
+    };
+
+    const handleBlur = () => {
+      setIsFocused(false);
+    };
+
+    return (
+      <div className={`relative sm:w-130 md:w-149 lg:w-379 ${className}`}>
+        <input
+          type="text"
+          className={`h-full w-full cursor-pointer rounded-4 border border-gray-700 py-5 focus:outline-none sm:h-44 sm:px-12 md:h-56 md:px-16 ${className} ${isFocused ? 'border-green-300' : ''}`}
+          onClick={handleFocus}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          value={value}
+          ref={ref}
+          placeholder={placeholder}
+          readOnly
+        />
+        <div className="pointer-events-none absolute top-1/2 -translate-y-1/2 sm:right-8 md:right-15">
+          <Image src="/assets/icons/calendar.svg" alt="calendar" width={24} height={24} />
+        </div>
       </div>
-    </div>
-  ),
+    );
+  },
 );
 
-CustomInput.displayName = 'CustomInput';
-
 export default CustomInput;
+
+CustomInput.displayName = 'CustomInput';
