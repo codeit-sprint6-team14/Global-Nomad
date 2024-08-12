1a394657e3c623bc7e01b79ca5bd990a06825695
YeongTaek <yeongtaek2109@gmail.com>
2024-08-12 22:32:18 +0900

    [FP-66] Design: ActivityCard UI 완성

--- src/components/pages/main/ActivityCard.tsx
+++ src/components/pages/main/ActivityCard.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+
+import Star from '../../../../public/assets/icons/star.svg';
 import { Activity } from './mainPage.type';
 
 interface ActivityCardProps {
@@ -6,16 +9,23 @@ interface ActivityCardProps {
 
 const ActivityCard: React.FC<ActivityCardProps> = ({ activity }) => {
   return (
-    <div className="activity-card">
-      <img src={activity.bannerImageUrl} alt={activity.title} />
-      <h3>{activity.title}</h3>
-      <p>{activity.description}</p>
-      <p>카테고리: {activity.category}</p>
-      <p>가격: {activity.price}원</p>
-      <p>주소: {activity.address}</p>
-      <p>
-        평점: {activity.rating} (리뷰 {activity.reviewCount}개)
-      </p>
+    <div className="flex h-414 w-283 flex-col gap-18">
+      <Image src={activity.bannerImageUrl} alt="banner" width={283} height={283} className="rounded-25" />
+      <div className="flex flex-col gap-15">
+        <div className="flex flex-col gap-10">
+          <div className="flex items-center gap-5">
+            <Star width={20} height={20} />
+            <span className="text-lg-medium text-black">{activity.rating}</span>
+            <span className="text-lg-medium text-gray-600">({activity.reviewCount})</span>
+          </div>
+          <p className="w-282 truncate text-2xl-semibold text-black">{activity.title}</p>
+        </div>
+        <div className="flex gap-5">
+          <span className="text-2xl-bold text-black">\ {activity.price}</span>
+          <span className="text-xl-regular text-gray-800">/</span>
+          <span className="text-xl-regular text-gray-600">인</span>
+        </div>
+      </div>
     </div>
   );
 };
