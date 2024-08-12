cbd369f42c4bead9876fe83ef9e3ca1aa27b61da
Lin <heron6534@gmail.com>
2024-08-12 14:28:07 +0900

    [FP-70] Refactor: DropdownInput 스타일 및 기능 리팩토링

--- src/components/common/Input/DropdownInput/dropdownInputItem.tsx
+++ src/components/common/Input/DropdownInput/dropdownInputItem.tsx
@@ -1,6 +1,7 @@
+import { dropdownTypeAtom } from '@/store/dropdownAtom';
 import { Option } from '@/types/dropDownInputTypes';
+import { useAtomValue } from 'jotai';
 import Image from 'next/image';
-import React from 'react';
 
 interface DropdownItemProps {
   option: Option;
@@ -8,18 +9,24 @@ interface DropdownItemProps {
   onClick: () => void;
 }
 
-const DropdownItem = ({ option, isSelected, onClick }: DropdownItemProps) => (
-  <li
-    className={`m-5 flex cursor-pointer items-center rounded-4 p-8 ${
-      isSelected ? 'bg-black-100 text-white' : 'hover:bg-black-100 hover:text-white'
-    }`}
-    onClick={onClick}
-  >
-    <div className="mx-8 w-14">
-      {isSelected && <Image src="/assets/icons/checked.svg" width={13} height={13} alt="checked icon" />}
-    </div>
-    <span>{option.label}</span>
-  </li>
-);
+const DropdownItem = ({ option, isSelected, onClick }: DropdownItemProps) => {
+  const dropDownType = useAtomValue(dropdownTypeAtom);
+
+  return (
+    <li
+      className={`m-5 flex cursor-pointer items-center rounded-4 p-8 ${
+        isSelected ? 'bg-black-100 text-white' : 'hover:bg-black-100 hover:text-white'
+      } ${dropDownType === 'time' ? 'justify-center' : ''}`}
+      onClick={onClick}
+    >
+      {dropDownType === 'category' && (
+        <div className="mx-8 w-14">
+          {isSelected && <Image src="/assets/icons/checked.svg" width={13} height={13} alt="checked icon" />}
+        </div>
+      )}
+      <span>{option.label}</span>
+    </li>
+  );
+};
 
 export default DropdownItem;
