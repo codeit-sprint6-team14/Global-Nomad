7f5e309a828d03cc3001b2a86393be98097e1827
Lin <heron6534@gmail.com>
2024-08-13 12:29:52 +0900

    [FP-70] Chore: 띄어쓰기 수정

--- src/constants/registAcitivtyErrorMessages.js
+++ src/constants/registAcitivtyErrorMessages.js
@@ -1,15 +1,15 @@
 const REGIST_ACTIVITY_ERROR_MESSAGES = {
-  TITLE_REQUIRED: '제목을 입력해주세요',
-  CATEGORY_REQUIRED: '카테고리를 선택해주세요',
-  DESCRIPTION_REQUIRED: '설명을 입력해주세요',
-  PRICE_REQUIRED: '가격을 입력해주세요',
-  ADDRESS_REQUIRED: '주소를 입력해주세요',
-  DATE_REQUIRED: '날짜를 선택해주세요',
-  START_TIME_REQUIRED: '시작 시간을 선택해주세요',
-  END_TIME_REQUIRED: '종료 시간을 선택해주세요',
-  MIN_TIME_SLOTS: '최소 1개의 시간대를 추가해주세요',
-  BANNER_IMAGE_REQUIRED: '배너 이미지를 1개 이상 등록해주세요.',
-  INTRO_IMAGES_REQUIRED: '소개 이미지를 1개 이상 등록해주세요.',
+  TITLE_REQUIRED: '제목을 입력해 주세요',
+  CATEGORY_REQUIRED: '카테고리를 선택해 주세요',
+  DESCRIPTION_REQUIRED: '설명을 입력해 주세요',
+  PRICE_REQUIRED: '가격을 입력해 주세요',
+  ADDRESS_REQUIRED: '주소를 입력해 주세요',
+  DATE_REQUIRED: '날짜를 선택해 주세요',
+  START_TIME_REQUIRED: '시작 시간을 선택해 주세요',
+  END_TIME_REQUIRED: '종료 시간을 선택해 주세요',
+  MIN_TIME_SLOTS: '최소 1개의 시간대를 추가해 주세요',
+  BANNER_IMAGE_REQUIRED: '배너 이미지를 1개 이상 등록해 주세요.',
+  INTRO_IMAGES_REQUIRED: '소개 이미지를 1개 이상 등록해 주세요.',
 };
 
 export default REGIST_ACTIVITY_ERROR_MESSAGES;
