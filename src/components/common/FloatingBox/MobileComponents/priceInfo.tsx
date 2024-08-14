454d6b1f06c3bbee93fdccb3935e8779929be1b4
hoon <dudrn587@gmail.com>
2024-08-14 13:38:32 +0900

    [FP-76] Design: 체험 상세 페이지 모바일 일정 선택 시 가격, 일정 렌더링 추가

--- src/components/common/FloatingBox/MobileComponents/priceInfo.tsx
+++ src/components/common/FloatingBox/MobileComponents/priceInfo.tsx
@@ -1,13 +1,19 @@
-interface PriceInfoProps {
-  price: number;
-  capacity: number;
-}
+import { ReservationPriceAtom, headCountAtom, selectedScheduleStateAtom } from '@/store/activityDetailsAtom';
+import { useAtomValue } from 'jotai';
 
-const PriceInfo = ({ price, capacity }: PriceInfoProps) => {
+const PriceInfo = () => {
+  const isSelectedSchedule = useAtomValue(selectedScheduleStateAtom);
+  const price = useAtomValue(ReservationPriceAtom);
+  const headCount = useAtomValue(headCountAtom);
+
   return (
     <div className="flex items-center">
-      <span className="mr-6 text-xl-bold text-black-100">₩ {price.toLocaleString()}</span>
-      <span className="text-2lg-medium text-green-300"> / 총 {capacity}인</span>
+      <span className="mr-6 text-xl-bold text-black-100">
+        ₩ {isSelectedSchedule ? price * headCount : price.toLocaleString()}
+      </span>
+      <span className="flex items-center text-2lg-medium text-green-300">
+        {isSelectedSchedule ? `/ 총 ${headCount}인` : '/ 1인'}
+      </span>
     </div>
   );
 };
--- src/components/common/Modal/DateSelect/footer.tsx
+++ src/components/common/Modal/DateSelect/footer.tsx
@@ -1,14 +1,25 @@
 import Button from '@/components/common/Button';
+import { selectedScheduleStateAtom } from '@/store/activityDetailsAtom';
+import { SetStateAction, useSetAtom } from 'jotai';
+import { Dispatch } from 'react';
 
 interface FooterProps {
-  isReservationPossible: boolean;
+  // isReservationPossible: boolean;
+  setIsModalOpen?: Dispatch<SetStateAction<boolean>>;
 }
+// prop -> isReservationPossible 일정 선택 시 버튼 활성화 기능 구현 예정
+const Footer = ({ setIsModalOpen }: FooterProps) => {
+  const setIsSelectedSchedule = useSetAtom(selectedScheduleStateAtom);
 
-const Footer = ({ isReservationPossible }: FooterProps) => {
+  const handleScheduleSelect = () => {
+    setIsModalOpen?.(false);
+    setIsSelectedSchedule(true);
+  };
+
   return (
     <div>
       <footer>
-        <Button.Default disabled={!isReservationPossible} className="h-56 w-full">
+        <Button.Default /*disabled={!isReservationPossible}*/ onClick={handleScheduleSelect} className="h-56 w-full">
           확인
         </Button.Default>
       </footer>
