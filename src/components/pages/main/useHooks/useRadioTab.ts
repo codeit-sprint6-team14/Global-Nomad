4aca012128f6b4c462ac5dc535957624cb5586cc
YeongTaek <yeongtaek2109@gmail.com>
2024-08-17 02:04:29 +0900

    [FP-66] Remove: 파일 삭제

--- src/components/pages/main/useHooks/useRadioTab.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { RadioTabContext } from '../RadioTab/RadioTabContext';
-
-export const useRadioTab = () => {
-  const context = useContext(RadioTabContext);
-  if (!context) throw new Error('useRadioTab must be used within a RadioTabRoot');
-  return context;
-};
