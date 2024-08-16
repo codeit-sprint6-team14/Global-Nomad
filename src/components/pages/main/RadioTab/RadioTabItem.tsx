b0f74b5338c53709bf1ae805e1972c86b4f0c337
YeongTaek <yeongtaek2109@gmail.com>
2024-08-17 02:24:00 +0900

    [FP-66] Refactor: RadioTab 클릭시 스타일 반영 및 필터링 동작 개선

--- src/components/pages/main/RadioTab/RadioTabItem.tsx
+++ src/components/pages/main/RadioTab/RadioTabItem.tsx
@@ -15,17 +15,13 @@ export const RadioTabItem: React.FC<RadioTabItemProps> = ({ id, children }) => {
 
   return (
     <label
-      className={`flex h-58 w-127 cursor-pointer items-center justify-center rounded-15 border border-black-100 text-black-100 hover:bg-black-100 hover:text-white ${activeTab === id ? 'bg-black-100' : 'bg-white'}`}
+      onClick={() => setActiveTab(id)}
+      className={`flex h-58 w-127 cursor-pointer items-center justify-center rounded-15 border border-black-100 text-black-100 transition-colors duration-200 ease-in-out hover:bg-black-100 hover:text-white ${
+        activeTab === id ? 'bg-black-100 text-white' : 'bg-white hover:text-black-100'
+      } `}
     >
-      <input
-        type="radio"
-        name="radio-tab"
-        value={id}
-        checked={activeTab === id}
-        onChange={() => setActiveTab(id)}
-        className="sr-only text-white"
-      />
-      <span className={`text-2lg-medium ${activeTab === id ? 'text-white' : ''}`}>{children}</span>
+      <input type="radio" name="radio-tab" value={id} checked={activeTab === id} className="sr-only" />
+      <span className="text-2lg-medium">{children}</span>
     </label>
   );
 };
--- src/components/pages/main/RadioTab/RadtioTabRoot.tsx
+++ src/components/pages/main/RadioTab/RadtioTabRoot.tsx
@@ -1,14 +1,19 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 import { RadioTabContext } from './RadioTabContext';
 
 type RadioTabRootProps = {
   children: ReactNode;
   defaultTab: string;
+  onTabChange: (tab: string) => void;
 };
 
-export const RadioTabRoot: React.FC<RadioTabRootProps> = ({ children, defaultTab }) => {
+export const RadioTabRoot: React.FC<RadioTabRootProps> = ({ children, defaultTab, onTabChange }) => {
   const [activeTab, setActiveTab] = useState(defaultTab);
+
+  useEffect(() => {
+    onTabChange(activeTab);
+  }, [activeTab, onTabChange]);
 
   return <RadioTabContext.Provider value={{ activeTab, setActiveTab }}>{children}</RadioTabContext.Provider>;
 };
