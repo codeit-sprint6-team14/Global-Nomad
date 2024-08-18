41a94776d8052d0e0720ad18f0aa0512711a961d
Lin <heron6534@gmail.com>
2024-08-18 20:28:05 +0900

    [FP-71] Feat: RegisterConfirm 컴포넌트 닫기 기능 추가

--- src/components/common/Modal/RegisterConfirm/index.tsx
+++ src/components/common/Modal/RegisterConfirm/index.tsx
@@ -2,15 +2,19 @@ import Button from '@/components/common/Button';
 
 type PopupProps = {
   children: React.ReactNode;
+  onClose: () => void;
 };
 
-const RegisterConfirmPopup = ({ children }: PopupProps) => {
+const RegisterConfirmPopup = ({ onClose, children }: PopupProps) => {
   return (
-    <div className="relative flex h-220 w-327 flex-col items-center rounded-8 border md:h-250 md:w-540">
+    <div className="relative flex h-220 w-327 flex-col items-center rounded-8 border bg-white md:h-250 md:w-540">
       <span className="absolute top-[35%] text-lg-medium text-black-100 md:top-[40%] md:text-2lg-medium">
         {children}
       </span>
-      <Button.Default className="absolute bottom-20 right-[30%] h-42 sm:w-138 md:right-30 md:h-48 md:w-120">
+      <Button.Default
+        className="absolute bottom-20 right-[30%] h-42 sm:w-138 md:right-30 md:h-48 md:w-120"
+        onClick={onClose}
+      >
         확인
       </Button.Default>
     </div>
