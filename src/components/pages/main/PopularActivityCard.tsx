5d696da6c0b13c1a8f802068973d7f5b8eee44e6
YeongTaek <yeongtaek2109@gmail.com>
2024-08-20 22:59:14 +0900

    [FP-66] Design: PopularActivityCard 반응형 UI

--- src/components/pages/main/PopularActivityCard.tsx
+++ src/components/pages/main/PopularActivityCard.tsx
@@ -6,7 +6,10 @@ import { ActivityCardProps } from './mainPage.type';
 
 const PopularActivityCard: React.FC<ActivityCardProps> = ({ activity }) => {
   return (
-    <Link href={`/activities/${activity.id}`} className="group relative mb-48 h-384 w-384 overflow-hidden rounded-25">
+    <Link
+      href={`/activities/${activity.id}`}
+      className="group relative mb-48 overflow-hidden rounded-25 sm:h-168 sm:w-168 md:h-384 md:w-384"
+    >
       <Image
         src={activity.bannerImageUrl}
         alt="PopularActivityThumnail"
@@ -14,15 +17,15 @@ const PopularActivityCard: React.FC<ActivityCardProps> = ({ activity }) => {
         height={384}
         className="absolute"
       />
-      <div className="absolute bottom-0 flex flex-col justify-end gap-20 px-20 py-30 text-white">
+      <div className="absolute bottom-0 flex flex-col justify-end px-20 pb-12 pt-30 text-white sm:gap-6 md:gap-20 md:py-30">
         <div className="flex items-center gap-5 text-md-semibold">
           <Star width={20} height={20} />
           <span>{activity.rating}</span>
           <span>({activity.reviewCount})</span>
         </div>
-        <p className="text-3xl-bold">{activity.title}</p>
+        <p className="line-clamp-2 text-wrap break-words sm:text-2lg-bold md:text-3xl-bold">{activity.title}</p>
         <div className="flex items-center gap-5">
-          <span className="text-xl-bold">\ {activity.price.toLocaleString()}</span>
+          <span className="sm:text-lg-bold md:text-xl-bold">\ {activity.price.toLocaleString()}</span>
           <span className="text-md-regular text-gray-600">/인</span>
         </div>
       </div>
