cd834032eac19a927785a5d900b59cce83df0767
Lin <heron6534@gmail.com>
2024-08-22 07:44:06 +0900

    [FP-85] Refactor: 수정한 ModalOverlay 컴포넌트 적용

--- src/pages/my-page/reservation-list.tsx
+++ src/pages/my-page/reservation-list.tsx
@@ -4,12 +4,10 @@ import NavBar from '@/components/common/NavBar';
 import SideNavMenu from '@/components/common/SideNavMenu';
 import { useMyReservationsQuery } from '@/components/pages/myPage/ReservationList/hooks/useMyReservationsQuery';
 import ReservationContent from '@/components/pages/myPage/ReservationList/reservationContent';
-import { useClickOutside } from '@/hooks/useClickOutside';
 import { useDeviceState } from '@/hooks/useDeviceState';
 import { modalAtom } from '@/store/modalAtom';
 import { Device } from '@/types/deviceTypes';
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
 
 const ReservationList = () => {
   const [modalType, setModalType] = useAtom(modalAtom);
@@ -30,30 +28,6 @@ const ReservationList = () => {
   const handleCloseModal = () => {
     setModalType(null);
   };
-
-  const modalRef = useClickOutside(handleCloseModal);
-
-  const getScrollbarWidth = () => {
-    return window.innerWidth - document.documentElement.clientWidth;
-  };
-
-  useEffect(() => {
-    const scrollbarWidth = getScrollbarWidth();
-
-    // 모달 열려있는 경우 스크롤바 숨기는 로직
-    if (modalType) {
-      document.body.style.overflow = 'hidden';
-      document.body.style.paddingRight = `${scrollbarWidth}px`;
-    } else {
-      document.body.style.overflow = 'auto';
-      document.body.style.paddingRight = '0px';
-    }
-
-    return () => {
-      document.body.style.overflow = 'auto';
-      document.body.style.paddingRight = '0px';
-    };
-  }, [modalType]);
 
   return (
     <>
@@ -84,28 +58,20 @@ const ReservationList = () => {
             </div>
           </div>
         )}
-        {modalType === 'cancel' && (
-          <Modal.Overlay>
-            <div ref={modalRef}>
-              <Modal.CancelConfirm />
-            </div>
-          </Modal.Overlay>
-        )}
-        {modalType === 'review' && (
-          <Modal.Overlay>
-            <div ref={modalRef} className="h-full w-full md:h-auto md:w-auto">
-              <Modal.Review
-                title="함께 배우면 즐거운 스트릿 댄스"
-                bannerImageUrl="/images/test-profile-img.png"
-                date="2023. 2. 14"
-                startTime="11:00"
-                endTime="12:30"
-                totalPrice={10000}
-                headCount={10}
-              />
-            </div>
-          </Modal.Overlay>
-        )}
+        <Modal.Overlay isOpen={modalType !== null} onClose={handleCloseModal}>
+          {modalType === 'cancel' && <Modal.CancelConfirm />}
+          {modalType === 'review' && (
+            <Modal.Review
+              title="함께 배우면 즐거운 스트릿 댄스"
+              bannerImageUrl="/images/test-profile-img.png"
+              date="2023. 2. 14"
+              startTime="11:00"
+              endTime="12:30"
+              totalPrice={10000}
+              headCount={10}
+            />
+          )}
+        </Modal.Overlay>
         <div ref={setTarget}></div>
       </main>
       <Footer />
