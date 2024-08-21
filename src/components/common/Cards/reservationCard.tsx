626d4fa8bab81adeae79535548ef6218cad0146a
Lin <heron6534@gmail.com>
2024-08-21 14:43:58 +0900

    [FP-81] feat: ReservationCard 컴포넌트 리뷰 완료 상태 처리 추가
    - 리뷰 완료 시 버튼 숨김 처리

--- src/components/common/Cards/reservationCard.tsx
+++ src/components/common/Cards/reservationCard.tsx
@@ -1,28 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { MyReservation } from '@/apis/myPage/myReservations.types';
 import Card from '@/components/common/Cards/components';
+import { completedReviewsAtom } from '@/store/completedReviewAtom';
 import { modalAtom } from '@/store/modalAtom';
 import { reservationIdAtom } from '@/store/reservationIdAtom';
 import { StatusType, getButtonInfo, getCardStatus } from '@/utils/cardStatus';
 import { formatPrice } from '@/utils/formatPrice';
-import { useSetAtom } from 'jotai';
+import { useAtom, useSetAtom } from 'jotai';
 
 const ReservationCard = ({ myReservationData }: { myReservationData: MyReservation }) => {
-  const setModalType = useSetAtom(modalAtom);
+  const [modalType, setModalType] = useAtom(modalAtom);
   const setReservationId = useSetAtom(reservationIdAtom);
+  const [completedReviews] = useAtom(completedReviewsAtom);
 
   const { id: reservationId, activity, status, totalPrice, headCount, date, startTime, endTime } = myReservationData;
   const { id: activityId, bannerImageUrl, title } = activity;
-
   const { text: statusText, colorClass } = getCardStatus(status as StatusType);
   const { name: buttonName, action } = getButtonInfo(status as StatusType);
 
+  const isReviewCompleted = completedReviews.includes(reservationId);
+
   const handleButtonClick = () => {
-    if (action !== null) {
+    if (action !== null && !isReviewCompleted) {
       setReservationId(reservationId);
       setModalType(action);
     }
   };
+
   return (
     <>
       <Card>
@@ -30,12 +34,16 @@ const ReservationCard = ({ myReservationData }: { myReservationData: MyReservati
         <Card.Header ClassNames={`${colorClass} text-md-bold md:text-lg-bold `} text={statusText} />
         <Card.Title title={title} />
         <Card.Body text={`${date} · ${startTime} - ${endTime} · ${headCount}명`} />
-        <Card.Footer
-          text={formatPrice(totalPrice)}
-          status={status}
-          buttonName={buttonName}
-          onButtonClick={handleButtonClick}
-        />
+        {!isReviewCompleted ? (
+          <Card.Footer
+            text={formatPrice(totalPrice)}
+            status={status}
+            buttonName={buttonName}
+            onButtonClick={handleButtonClick}
+          />
+        ) : (
+          <Card.Footer text={formatPrice(totalPrice)} status={status} />
+        )}
       </Card>
     </>
   );
