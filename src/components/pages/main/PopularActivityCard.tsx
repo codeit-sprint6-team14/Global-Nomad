0d9c9cd194ed68bb9bfc10f4f4c33f7072d9f65c
YeongTaek <yeongtaek2109@gmail.com>
2024-08-22 18:17:39 +0900

    [FP-66] Refactor: useBackupImage 작성 및 적용

--- src/components/pages/main/PopularActivityCard.tsx
+++ src/components/pages/main/PopularActivityCard.tsx
@@ -1,40 +1,19 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useMemo, useState } from 'react';
 
 import Star from '../../../../public/assets/icons/star.svg';
-import BasicThumbnail1 from '../../../../public/assets/images/testImg/air-balloon.png';
-import BasicThumbnail2 from '../../../../public/assets/images/testImg/dance.png';
-import BasicThumbnail3 from '../../../../public/assets/images/testImg/dog.png';
-import BasicThumbnail4 from '../../../../public/assets/images/testImg/fish.png';
-import BasicThumbnail5 from '../../../../public/assets/images/testImg/village.png';
 import { ActivityCardProps } from './mainPage.type';
-
-const backupThumbnails = [BasicThumbnail1, BasicThumbnail2, BasicThumbnail3, BasicThumbnail4, BasicThumbnail5];
+import { useBackupImage } from './useBackupImage';
 
 const PopularActivityCard: React.FC<ActivityCardProps> = ({ activity }) => {
-  const [imageLoadError, setImageLoadError] = useState(false);
-
-  const randomBackupThumbnail = useMemo(() => {
-    return backupThumbnails[Math.floor(Math.random() * backupThumbnails.length)];
-  }, []);
-
-  const handleImageError = () => {
-    setImageLoadError(true);
-  };
+  const { imageSource, altText, handleImageError } = useBackupImage(activity.bannerImageUrl);
 
   return (
     <Link
       href={`/activities/${activity.id}`}
       className="group relative mb-48 overflow-hidden rounded-25 sm:h-168 sm:w-168 md:h-384 md:w-384"
     >
-      <Image
-        src={activity.bannerImageUrl && !imageLoadError ? activity.bannerImageUrl : randomBackupThumbnail}
-        alt={imageLoadError ? 'Backup Popular Activity Thumbnail' : 'Popular Activity Thumbnail'}
-        layout="fill"
-        objectFit="cover"
-        onError={handleImageError}
-      />
+      <Image src={imageSource} alt={altText} layout="fill" objectFit="cover" onError={handleImageError} />
       <div className="absolute inset-0 flex flex-col justify-end p-4 text-white">
         <div className="flex items-center gap-2">
           <Star width={16} height={16} />
---
+++ src/components/pages/main/useBackupImage.ts
@@ -0,0 +1,33 @@
+import { StaticImageData } from 'next/image';
+import { useMemo, useState } from 'react';
+
+import BasicThumbnail1 from '../../../../public/assets/images/testImg/air-balloon.png';
+import BasicThumbnail2 from '../../../../public/assets/images/testImg/dance.png';
+import BasicThumbnail3 from '../../../../public/assets/images/testImg/dog.png';
+import BasicThumbnail4 from '../../../../public/assets/images/testImg/fish.png';
+import BasicThumbnail5 from '../../../../public/assets/images/testImg/village.png';
+
+const backupThumnails: StaticImageData[] = [
+  BasicThumbnail1,
+  BasicThumbnail2,
+  BasicThumbnail3,
+  BasicThumbnail4,
+  BasicThumbnail5,
+];
+
+export const useBackupImage = (originalImageUrl: string | undefined) => {
+  const [imageLoadError, setImageLoadError] = useState(false);
+
+  const backupImage = useMemo(() => {
+    return backupThumnails[Math.floor(Math.random() * backupThumnails.length)];
+  }, []);
+
+  const handleImageError = () => {
+    setImageLoadError(true);
+  };
+
+  const imageSource = originalImageUrl && !imageLoadError ? originalImageUrl : backupImage.src;
+  const altText = imageLoadError ? 'Backup Thumbnail' : 'Activity Thumbnail';
+
+  return { imageSource, altText, handleImageError };
+};
