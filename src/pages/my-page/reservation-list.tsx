76ed00df99394d08640380fa0e4ee4e6054320b7
Lin <heron6534@gmail.com>
2024-08-22 10:39:31 +0900

    [FP-85] Feat: 예약 내역 페이지 프레이머 모션 추가

--- src/pages/my-page/reservation-list.tsx
+++ src/pages/my-page/reservation-list.tsx
@@ -1,3 +1,4 @@
+import AnimatedContainer from '@/components/common/Animation/AnimatedContainer';
 import Footer from '@/components/common/Footer';
 import Modal from '@/components/common/Modal';
 import NavBar from '@/components/common/NavBar';
@@ -32,48 +33,50 @@ const ReservationList = () => {
   return (
     <>
       <NavBar />
-      <main className="md:mb-400 lg:mb-270 mx-auto mb-[496px] mt-94 w-344 md:w-696 lg:mb-142 lg:mt-142 lg:w-1200">
-        {queryError && <div className="mb-4 text-red-500">{queryError.message}</div>}
-        {isMobile && (
-          <div>
-            <ReservationContent
-              myReservationsData={myReservationsData}
-              isEmptyMyReservationData={isMyReservationsEmpty}
-              onStatusChange={handleStatusChange}
-              selectedStatus={selectedStatus}
-            />
-          </div>
-        )}
-        {(isTablet || isDesktop) && (
-          <div className="flex justify-between">
-            <SideNavMenu />
+      <AnimatedContainer>
+        <main className="md:mb-400 lg:mb-270 mx-auto mb-[496px] mt-94 w-344 md:w-696 lg:mb-142 lg:mt-142 lg:w-1200">
+          {queryError && <div className="mb-4 text-red-500">{queryError.message}</div>}
+          {isMobile && (
             <div>
               <ReservationContent
                 myReservationsData={myReservationsData}
-                showFilter={isDesktop}
                 isEmptyMyReservationData={isMyReservationsEmpty}
                 onStatusChange={handleStatusChange}
                 selectedStatus={selectedStatus}
               />
             </div>
-          </div>
-        )}
-        <Modal.Overlay isOpen={modalType !== null} onClose={handleCloseModal}>
-          {modalType === 'cancel' && <Modal.CancelConfirm />}
-          {modalType === 'review' && (
-            <Modal.Review
-              title="함께 배우면 즐거운 스트릿 댄스"
-              bannerImageUrl="/images/test-profile-img.png"
-              date="2023. 2. 14"
-              startTime="11:00"
-              endTime="12:30"
-              totalPrice={10000}
-              headCount={10}
-            />
           )}
-        </Modal.Overlay>
-        <div ref={setTarget}></div>
-      </main>
+          {(isTablet || isDesktop) && (
+            <div className="flex justify-between">
+              <SideNavMenu />
+              <div>
+                <ReservationContent
+                  myReservationsData={myReservationsData}
+                  showFilter={isDesktop}
+                  isEmptyMyReservationData={isMyReservationsEmpty}
+                  onStatusChange={handleStatusChange}
+                  selectedStatus={selectedStatus}
+                />
+              </div>
+            </div>
+          )}
+          <Modal.Overlay isOpen={modalType !== null} onClose={handleCloseModal}>
+            {modalType === 'cancel' && <Modal.CancelConfirm />}
+            {modalType === 'review' && (
+              <Modal.Review
+                title="함께 배우면 즐거운 스트릿 댄스"
+                bannerImageUrl="/images/test-profile-img.png"
+                date="2023. 2. 14"
+                startTime="11:00"
+                endTime="12:30"
+                totalPrice={10000}
+                headCount={10}
+              />
+            )}
+          </Modal.Overlay>
+          <div ref={setTarget}></div>
+        </main>
+      </AnimatedContainer>
       <Footer />
     </>
   );
