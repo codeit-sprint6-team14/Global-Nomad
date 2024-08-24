a56d75d2e61cea735558c0f1fe209ef70702190c
Lin <heron6534@gmail.com>
2024-08-24 10:53:09 +0900

    [FP-86] Design: Design: 예약 내역 페이지 모바일 사이즈 뒤로가기 버튼 추가

--- src/components/pages/myPage/ReservationList/reservationContent.tsx
+++ src/components/pages/myPage/ReservationList/reservationContent.tsx
@@ -1,32 +1,16 @@
 import { MyReservation } from '@/apis/myPage/myReservations.types';
-import Filter from '@/components/common/Filter';
 
 import EmptyContent from './emptyContent';
 import ReservationCardList from './reservationCardList';
 
 interface ReservationContentProps {
   myReservationsData: MyReservation[];
-  showFilter?: boolean;
   isEmptyMyReservationData: boolean;
-  onStatusChange: (newStatus: string) => void;
-  selectedStatus: string | null;
 }
 
-const ReservationContent = ({
-  myReservationsData,
-  showFilter = false,
-  isEmptyMyReservationData = false,
-  onStatusChange,
-  selectedStatus,
-}: ReservationContentProps) => {
+const ReservationContent = ({ myReservationsData, isEmptyMyReservationData = false }: ReservationContentProps) => {
   return (
     <>
-      <div
-        className={`mb-12 md:mb-24 lg:mb-16 lg:flex lg:items-center lg:justify-between ${isEmptyMyReservationData && 'mb-86 md:mb-82 lg:mb-[117px]'}`}
-      >
-        <h1 className="text-3xl-bold">예약 내역</h1>
-        {showFilter && <Filter content="필터" onOptionSelect={onStatusChange} selectedOption={selectedStatus} />}
-      </div>
       {isEmptyMyReservationData ? (
         <div className="mb-420 md:mb-601 md:w-429 lg:mb-[469px] lg:w-792">
           <EmptyContent />
