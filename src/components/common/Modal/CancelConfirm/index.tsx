45ad26c0615c94c89db9d2795d10dc2f93609ad9
Lin <heron6534@gmail.com>
2024-10-12 09:22:01 +0900

    refactor(CancelConfirmPopup): usePatchMyReservationMutation 훅을 useReservationMutation으로 변경 및 함수명 개선

--- src/components/common/Modal/CancelConfirm/index.tsx
+++ src/components/common/Modal/CancelConfirm/index.tsx
@@ -1,5 +1,5 @@
 import Button from '@/components/common/Button';
-import { usePatchMyReservationMutation } from '@/components/common/Cards/hooks/usePatchMyReservationMutation';
+import { useReservationMutation } from '@/components/common/Cards/hooks/useReservationMutation';
 import { modalAtom } from '@/store/modalAtom';
 import { reservationIdAtom } from '@/store/reservationIdAtom';
 import { useAtomValue, useSetAtom } from 'jotai';
@@ -8,23 +8,22 @@ import Image from 'next/image';
 const CancelConfirmPopup = () => {
   const setModalType = useSetAtom(modalAtom);
   const reservationId = useAtomValue(reservationIdAtom);
-
-  const { mutate: patchMyReservations, error: patchMyReservationError } = usePatchMyReservationMutation();
+  const { mutate, error } = useReservationMutation();
 
   const handleCloseModal = () => {
     setModalType(null);
   };
 
-  const handleSubmit = () => {
+  const handleCancelReservation = () => {
     if (reservationId) {
-      patchMyReservations(reservationId);
+      mutate(reservationId);
+      handleCloseModal();
     }
-    setModalType(null);
   };
 
   return (
     <>
-      {patchMyReservationError && <div className="mb-4 text-red-500">{patchMyReservationError.message}</div>}
+      {error && <div className="mb-4 text-red-500">{error.message}</div>}
       <div className="flex h-184 w-288 flex-col items-center justify-evenly rounded-12 border bg-white shadow-modal">
         <div className="flex flex-col items-center justify-between gap-16">
           <Image src="/assets/icons/check.svg" width={24} height={24} alt="체크 아이콘" />
@@ -34,7 +33,7 @@ const CancelConfirmPopup = () => {
           <Button.Default onClick={handleCloseModal} variant="secondary" className="h-38 w-80">
             아니오
           </Button.Default>
-          <Button.Default onClick={handleSubmit} type="submit" className="h-38 w-80">
+          <Button.Default onClick={handleCancelReservation} type="submit" className="h-38 w-80">
             취소하기
           </Button.Default>
         </div>
