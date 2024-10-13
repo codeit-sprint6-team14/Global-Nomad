c3229a976b80bab7ab6b02fa315d5eaf11168dbc
Lin <heron6534@gmail.com>
2024-10-13 14:59:52 +0900

    [FP-112] fix(storybook): 빌드 에러 해결

--- src/stories/Modal/reviewModal.stories.tsx
+++ src/stories/Modal/reviewModal.stories.tsx
@@ -1,3 +1,4 @@
+import { Reservation } from '@/apis/myPage/myReservations.types';
 import Modal from '@/components/common/Modal';
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
@@ -38,16 +39,32 @@ const Template: Story = {
   render: (args) => <Modal.Review {...args} />,
 };
 
+const baseSelectedCard: Reservation = {
+  id: 1,
+  scheduleId: 1,
+  teamId: 'team1',
+  userId: 1,
+  status: 'confirmed',
+  reviewSubmitted: false,
+  totalPrice: 10000,
+  headCount: 10,
+  date: '2023. 2. 14',
+  startTime: '11:00',
+  endTime: '12:30',
+  createdAt: '2023-02-14T11:00:00Z',
+  updatedAt: '2023-02-14T11:00:00Z',
+  activity: {
+    id: 1,
+    title: '함께 배우면 즐거운 스트릿 댄스',
+    bannerImageUrl: '/public/assets/images/testImg/air-balloon.png',
+  },
+};
+
 export const Default: Story = {
   ...Template,
   args: {
-    title: '함께 배우면 즐거운 스트릿 댄스',
-    bannerImageUrl: '/assets/images/testImg/dance.png',
-    date: '2023. 2. 14',
-    startTime: '11:00',
-    endTime: '12:30',
-    totalPrice: 10000,
-    headCount: 10,
+    selectedCard: baseSelectedCard,
+    onClose: () => console.log('Modal closed'),
   },
   parameters: {
     docs: {
@@ -61,14 +78,20 @@ export const Default: Story = {
 export const Secondary: Story = {
   ...Template,
   args: {
-    ...Default.args,
-    title: '해안가 마을에서 1주일 살아보기',
-    bannerImageUrl: '/assets/images/testImg/village.png',
-    date: '2024. 8. 2',
-    startTime: '14:00',
-    endTime: '16:30',
-    totalPrice: 360000,
-    headCount: 4,
+    selectedCard: {
+      ...baseSelectedCard,
+      activity: {
+        ...baseSelectedCard.activity,
+        title: '해안가 마을에서 1주일 살아보기',
+        bannerImageUrl: '/assets/images/testImg/village.png',
+      },
+      date: '2024. 8. 2',
+      startTime: '14:00',
+      endTime: '16:30',
+      totalPrice: 360000,
+      headCount: 4,
+    },
+    onClose: () => console.log('Modal closed'),
   },
   parameters: {
     backgrounds: { default: 'dark' },
