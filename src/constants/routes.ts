4109260ca27c2412511e5b8be66935dfdda0e3b1
Lin <heron6534@gmail.com>
2024-10-13 14:25:56 +0900

    [FP-112] feat(Route): 페이지별 라우트 상수로 분리

---
+++ src/constants/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  MAIN_PAGE: '/',
+  ACTIVITES: '/activites',
+  MY_PAGE: '/my-page',
+  RESERVATION_LIST: '/my-page/reservation-list',
+  ACTIVITY_SETTINGS: '/my-page/activity-settings',
+  REGIST_ACTIVITY: 'my-page/regist-activity',
+  SCHEDULE: '/my-page/schedule',
+};
