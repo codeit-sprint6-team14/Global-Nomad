f9b6cf70a034e3a3c86f97cb74a07acb9d102de2
Lin <heron6534@gmail.com>
2024-10-13 14:30:39 +0900

    [FP-112] chore: import 문 정리 및 재정렬

--- src/components/common/Cards/components/cardFooter.tsx
+++ src/components/common/Cards/components/cardFooter.tsx
@@ -1,10 +1,9 @@
 import Button from '@/components/common/Button';
+import DropDown from '@/components/common/Dropdown';
 import { useClickOutside } from '@/hooks/useClickOutside';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-
-import DropDown from '../../Dropdown';
 
 interface CardFooterProps {
   text: string;
